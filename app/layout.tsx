5b4a30241e83a5740c73f0e1a8d9754ad26787e3
acelcabangs123 <russellcabang3@gmail.com>
2026-04-27 22:08:28 +0800

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/kpi.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/kpi.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/kpi.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/kpi.png',
   },
 }
 
